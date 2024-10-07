5586018dde74513b47cf07b0f7de8921759750b2
PraveenKumarTV <160822105+PraveenKumarTV@users.noreply.github.com>
2024-10-07 07:28:58 -0700

    Update app.java

--- app.java
+++ app.java
@@ -1,16 +1,26 @@
 import java.util.*;
-class Person {
+
+// Define Displayable interface
+interface Displayable {
+    void displayInfo();
+}
+
+class Person implements Displayable {
     private String name;
-public Person(String name) {
+
+    public Person(String name) {
         this.name = name;
     }
-public String getName() {
+
+    public String getName() {
         return name;
     }
-public void displayInfo() {
-        System.out.println("Name: " + name);
-    }}
 
+    @Override
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+    }
+}
 
 class Student extends Person {
     private String email;
@@ -18,7 +28,8 @@ class Student extends Person {
     private String studentId;
     private double gpa;
     private double scholarshipAmount;
-public Student(String name, String email, int graduationYear, String studentId, double gpa, double scholarshipAmount) {
+
+    public Student(String name, String email, int graduationYear, String studentId, double gpa, double scholarshipAmount) {
         super(name);
         this.email = email;
         this.graduationYear = graduationYear;
@@ -27,6 +38,7 @@ public Student(String name, String email, int graduationYear, String studentId, 
         this.scholarshipAmount = scholarshipAmount;
     }
 
+    @Override
     public void displayInfo() {
         super.displayInfo();
         System.out.println("Email: " + email);
@@ -34,47 +46,50 @@ public Student(String name, String email, int graduationYear, String studentId, 
         System.out.println("GPA: " + gpa);
         System.out.println("Scholarship Amount: " + scholarshipAmount);
     }
-public String getStudentId() {
+
+    public String getStudentId() {
         return studentId;
     }
 }
 
-
 class Donor extends Person {
     private double donationAmount;
-    private Date donationDate;
+    // Removed donationDate for simplicity
 
     public Donor(String name, double donationAmount) {
         super(name);
         this.donationAmount = donationAmount;
-        //this.donationDate = donationDate;
     }
 
     @Override
     public void displayInfo() {
         super.displayInfo();
         System.out.println("Donation Amount: " + donationAmount);
-        System.out.println("Donation Date: " + donationDate);
+        // System.out.println("Donation Date: " + donationDate); // Removed for simplicity
     }
 }
-
 
 class AlumniEvent {
     private String eventName;
     private String date;
     private String location;
-    private List<Student> attendees;
+    private Student[] attendees;
+    private int maxAttendees;
+    private int currentAttendeeCount;
 
     public AlumniEvent(String eventName, String date, String location, int maxAttendees) {
         this.eventName = eventName;
         this.date = date;
         this.location = location;
-        this.attendees = new ArrayList<>(maxAttendees);
+        this.maxAttendees = maxAttendees;
+        this.attendees = new Student[maxAttendees];
+        this.currentAttendeeCount = 0;
     }
 
     public boolean addAttendee(Student student) {
-        if (attendees.size() < attendees.size()) {
-            attendees.add(student);
+        if (currentAttendeeCount < maxAttendees) {
+            attendees[currentAttendeeCount] = student;
+            currentAttendeeCount++;
             return true;
         }
         return false; // Event is full
@@ -85,71 +100,72 @@ class AlumniEvent {
         System.out.println("Date: " + date);
         System.out.println("Location: " + location);
         System.out.println("Attendees:");
-        for (Student attendee : attendees) {
-            attendee.displayInfo(); // Polymorphic call
+        for (int i = 0; i < currentAttendeeCount; i++) {
+            attendees[i].displayInfo(); // Polymorphic call
         }
     }
 }
 
-
 class ScholarshipManager {
-    private List<Student> students = new ArrayList<>();
-    private List<Donor> donors = new ArrayList<>();
+    private Student[] students = new Student[100]; // Maximum of 100 students
+    private Donor[] donors = new Donor[100]; // Maximum of 100 donors
+    private int studentCount = 0;
+    private int donorCount = 0;
 
     public void addStudent(Student s) {
-        students.add(s);
+        if (studentCount < students.length) {
+            students[studentCount] = s;
+            studentCount++;
+        }
     }
 
     public void addDonor(Donor d) {
-        donors.add(d);
+        if (donorCount < donors.length) {
+            donors[donorCount] = d;
+            donorCount++;
+        }
     }
 
     public Student getStudent(String id) {
-        for (Student s : students) {
-            if (s.getStudentId().equals(id)) {
-                return s;
+        for (int i = 0; i < studentCount; i++) {
+            if (students[i].getStudentId().equals(id)) {
+                return students[i];
             }
         }
         return null;
     }
 
     public Donor getDonor(String name) {
-        for (Donor d : donors) {
-            if (d.getName().equals(name)) {
-                return d;
+        for (int i = 0; i < donorCount; i++) {
+            if (donors[i].getName().equals(name)) {
+                return donors[i];
             }
         }
         return null;
     }
 }
 
-
 class AlumniEventApp {
     public static void main(String[] args) {
         AlumniEvent alumniEvent = new AlumniEvent("2024 Alumni Reunion", "Sep 25, 2024", "KK Auditorium", 5);
         ScholarshipManager manager = new ScholarshipManager();
 
-        
-        Student student1 = new Student("praveen", "praveen@gmail.com", 2020, "S001", 9.8, 1000.00);
-        Student student2 = new Student("raju", "raju@gmail.com", 2021, "S002", 9.9, 1500.00);
-        Student student3 = new Student("akthar", "akthar@gmail.com", 2022, "S003", 9.7, 2000.00);
+        Student student1 = new Student("Praveen", "praveen@gmail.com", 2020, "S001", 9.8, 1000.00);
+        Student student2 = new Student("Raju", "raju@gmail.com", 2021, "S002", 9.9, 1500.00);
+        Student student3 = new Student("Akthar", "akthar@gmail.com", 2022, "S003", 9.7, 2000.00);
 
-        
         alumniEvent.addAttendee(student1);
         alumniEvent.addAttendee(student2);
         alumniEvent.addAttendee(student3);
 
-        
         manager.addStudent(student1);
         manager.addStudent(student2);
         manager.addStudent(student3);
-        Donor d1=new Donor("96 reblend",5000);
+        Donor d1 = new Donor("96 Reblend", 5000);
         manager.addDonor(d1);
 
-        
         alumniEvent.displayEventDetails();
 
-        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter student ID to retrieve: ");
         String id = sc.next();
@@ -159,7 +175,7 @@ class AlumniEventApp {
         } else {
             System.out.println("Student not found.");
         }
-        
+
         // Example of retrieving a donor
         System.out.print("Enter donor name to retrieve: ");
         String donorName = sc.next();
