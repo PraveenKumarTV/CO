cab9180ff146ede113d23f564351a3411babe149
PraveenKumarTV <160822105+PraveenKumarTV@users.noreply.github.com>
2024-11-20 13:12:55 +0530

    Add files via upload

--- exp8a.java
+++ exp8a.java
@@ -1,4 +1,9 @@
 import java.util.*;
+import java.util.logging.FileHandler;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.*;
 class Patient{
     String name,id,addr,disease,gender,dob;int age;long phno;
     Patient(String a,String b,String c,String d,String e,String f,long g,int h){
@@ -30,24 +35,41 @@ class Patient{
     long getphno(){
         return phno;
     }
-
     public void describe(){
         System.out.println("Name: "+name+" ID: "+id+" address: "+addr+" Disease: "+disease+" DOB: "+dob+" phno: "+phno);
     }
+
+    public String toString(){
+        return "Name: "+name+" ID: "+id+" address: "+addr+" Disease: "+disease+" DOB: "+dob+" phno: "+phno;
+    }
 }
-
-
-
-
 public class exp8a {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        Patient ob1=new Patient("cde","123","madurai","fever","male","13/11/24",1234567890,25);
-        ArrayList<Patient> list=new ArrayList<Patient>();
+        Patient ob1=new Patient("cdetyu","123","madurai","fever","male","13/11/24",1234567890,25);
+        LogManager lm=LogManager.getLogManager();
+        Logger lg=Logger.getLogger("example3");
+        Stack<Integer> st=new Stack<>();
+        st.push(10);
+
+        
+        try{
+        FileHandler fh=new FileHandler("example3.txt",true);
+        lm.addLogger(lg);
+        lg.addHandler(fh);
+        fh.setFormatter(new SimpleFormatter());
+        lg.log(Level.INFO, ob1.toString());
+        fh.close();
+    }
+    catch(Exception e){
+        System.out.println("Exception: "+e);
+    }
+        /*ArrayList<Patient> list=new ArrayList<Patient>();
         list.add(ob1);
         list.add(ob1);
         list.add(ob1);
         System.out.println(list);
+
         Iterator<Patient> it=list.iterator();
         System.out.print("enter gender: ");
         String a=sc.next();
@@ -75,7 +97,7 @@ public class exp8a {
             if(p.getid().equals(a)){
                 p.describe();
             }
-        }
+        }*/
 
     }
     
